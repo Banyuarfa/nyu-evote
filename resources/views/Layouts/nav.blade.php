ddc78e43217474ddf9b1a2b8dbd858328d98a303
Banyu_sans <banyuarfa1122@gmail.com>
2024-12-24 07:40:32 +0700

    update dashboard style & responsiveness

--- resources/views/Layouts/nav.blade.php
+++ resources/views/Layouts/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="relative flex w-full items-center justify-between p-4 shadow-md md:p-5 lg:p-6">
     <label class="rounded-lg px-4 py-2 text-lg font-semibold transition-colors hover:bg-slate-300 md:hidden"><i
             class="fi fi-br-menu-burger grid"></i><input type="checkbox" id="burger" hidden /></label>
-    <ul id="menu" class="z-10 hidden flex-col absolute md:static items-center gap-4 rounded-lg p-4 md:flex md:flex-row border md:border-none">
+    <ul id="menu" class="z-50 hidden flex-col absolute md:static items-center gap-4 rounded-lg p-4 md:flex md:flex-row border md:border-none">
         <li><x-link to="/">Dashboard</x-link></li>
         <li><x-link to="/osis">Vote Osis</x-link></li>
         <li><x-link to="/mpk">Vote Mpk</x-link></li>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,11 @@
 {{-- php artisan serve --host=192.168.1.10 --port=8000 --}}
 @extends("Layouts.app")
 @section("content")
-    <dialog class="relative rounded-lg p-4">
+    <dialog class="relative z-50 rounded-lg p-4">
         <div>
             <main class="h-36 w-80 pt-1 text-center">
                 <audio src="assets/sounds/done_sound.mp3"></audio>
-                <video class="mx-auto h-24" loop autoplay src="assets/icons/checked-animation.mp4"></video>
+                <video class="mx-auto h-24" src="assets/icons/checked-animation.mp4"></video>
                 <p>Terimakasih sudah vote paslon pilihanmu!</p>
             </main>
             <footer class="float-end">
@@ -14,9 +14,37 @@
         </div>
     </dialog>
 
-    <section class="flex h-[calc(100vh_-_72px)] items-center justify-center gap-2 bg-slate-100">
-        <a href="/osis" class="rounded-lg bg-white px-8 py-4 text-sm hover:bg-slate-50">Vote OSIS</a>
-        <a href="/mpk" class="rounded-lg bg-white px-8 py-4 text-sm hover:bg-slate-50">Vote MPK</a>
+    <section class="relative flex h-[calc(100vh_-_72px)] flex-col bg-slate-100">
+        <div class="top-10 z-40 mx-auto my-8 lg:absolute lg:left-1/2 lg:m-0 lg:-translate-x-1/2">
+            <div class="flex justify-center gap-2">
+                <img class="h-12 md:h-14 lg:h-16" src="assets/img/smkn1-logo.webp" alt="">
+                <img class="h-12 md:h-14 lg:h-16" src="assets/img/osis-logo.png" alt="">
+            </div>
+            <p class="mt-1 text-center text-base font-semibold uppercase md:mt-2 md:text-lg lg:mt-4 lg:text-xl"><span
+                    class="block">E-vote</span>Pilih pasangan
+                calonmu!</p>
+        </div>
+        <div class="flex h-full w-full">
+            <div class="relative left-0 h-full w-1/2 overflow-hidden hover:bg-[rgb(100,116,139,.1)] lg:top-0">
+                <a href="/osis"
+                    class="absolute -left-24 w-[250%] transition-all hover:-left-20 hover:w-[270%] md:left-0 md:w-[150%] md:hover:left-2 md:hover:w-[160%] lg:-left-20 lg:hover:-left-14">
+                    <p
+                        class="absolute left-24 top-28 font-['Poppins'] text-6xl font-bold text-white md:top-40 md:text-8xl lg:left-72 lg:top-96 lg:text-9xl">
+                        OSIS</p>
+                    <img class="" src="assets/img/siluet-osis.png" alt="">
+                </a>
+            </div>
+            <div class="relative right-0 h-full w-1/2 overflow-hidden hover:bg-[rgb(100,116,139,.1)] lg:top-0">
+                <a href="/mpk"
+                    class="absolute -right-24 w-[250%] overflow-hidden transition-all hover:-right-20 hover:w-[270%] md:-right-8 md:w-[150%] md:hover:-right-6 md:hover:w-[160%] lg:-right-20 lg:hover:-right-14">
+                    <p
+                        class="absolute right-24 top-28 font-['Poppins'] text-6xl font-bold text-white md:top-40 md:text-8xl lg:right-72 lg:top-96 lg:text-9xl">
+                        MPK</p>
+                    <img class="" src="assets/img/siluet-mpk.png" alt="">
+                </a>
+            </div>
+
+        </div>
     </section>
     <script>
         const dialog = document.body.querySelector("dialog")
@@ -24,6 +52,8 @@
         const audio = document.body.querySelector("audio")
         if ({{ session("vote_an_osis") || session("vote_an_mpk") }}) {
             audio.play()
+            video.play()
+            video.loop = true
             dialog.showModal()
         }
 
