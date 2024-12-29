950839ca73f90c3df3a01d86cf26923511bbfa00
Banyu_sans <banyuarfa1122@gmail.com>
2024-12-29 10:11:09 +0700

    add: web title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name='csrf-token' content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
+        <title>Voting App SMKN 1 Jakarta</title>
 
         <!-- Icons -->
         <link rel='stylesheet'
